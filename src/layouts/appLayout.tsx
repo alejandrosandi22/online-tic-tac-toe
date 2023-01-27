fd764636cf38ace95a03236016d858097ee6d8e8
alesf22 <alesf22@gmail.com>
2023-01-27 10:59:17 -0600

    initial

--- src/layouts/appLayout.tsx
+++ src/layouts/appLayout.tsx
@@ -1,34 +1,41 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function AppLayout({ children, title }: AppLayoutProps) {
   return (
     <>
       <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet='utf-8' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          name='viewport'
+          content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
         />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
+        <meta
+          name='description'
+          content='Tic Tac Toe is a simple, two-players game in wich players take turns making a grid of 3x3 squares with X or O. The player who succeeds in placing three of their marks in a horizontal, vertical or diagonal row wins the game.'
+        />
+        <meta name='author' content='Alejandro Sandí' />
+        <meta
+          name='keywords'
+          content='tic tac toe, tres en raya, gato, multiplayer game, play with friends'
+        />
         <title>{title}</title>
-
-        <link rel="manifest" href="/manifest.json" />
+        <link rel='icon' href='/favicon.svg' type='svg/x-icon' />
+        <link rel='manifest' href='/manifest.json' />
         <link
-          href="/icons/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
+          href='/icons/favicon-16x16.png'
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
         />
         <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
+          href='/icons/favicon-32x32.png'
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
         />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#ffffff" />
+        <link rel='apple-touch-icon' href='/apple-icon.png'></link>
+        <meta name='theme-color' content='#ffffff' />
       </Head>
       {children}
     </>
